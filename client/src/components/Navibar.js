f64b919e6d017692b75538beefc68001d3a4adb9
Chelovekk <60159926+YaBydlo@users.noreply.github.com>
2021-07-08 16:41:34 +0300

    Navbar rework

--- client/src/components/Navibar.js
+++ client/src/components/Navibar.js
@@ -11,29 +11,54 @@ export default function Navibar() {
     const auth = useContext(AuthContext)
     
     const logoutHandler = event =>{
-        
         event.preventDefault()
         auth.logout()
         history.push('/')
     }
-
-    return(
+    if(auth.isAuthenticated){
+        return(
     
-        // <h1>AuthPage </h1>
-        <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
-            <Navbar.Brand>Logo</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link ><NavLink to="/">Home</NavLink></Nav.Link>
-                <Nav.Link ><NavLink to="/cabinet">Cabinet</NavLink></Nav.Link>
-                    <a  onClick={logoutHandler}><Nav.Link>logout</Nav.Link></a>
-                </Nav>
-                <Nav>
-                  <NavLink to="/auth"><Button variant="primary" className="mr-2">Log in</Button></NavLink>  
-                    <Button variant="primary">Sign in</Button>
-                </Nav> 
-            </Navbar.Collapse>
-        </Navbar>
-    )
+            // <h1>AuthPage </h1>
+            <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
+                <Navbar.Brand>Logo</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link ><NavLink to="/">Home</NavLink></Nav.Link>
+                    <Nav.Link ><NavLink to="/cabinet">Cabinet</NavLink></Nav.Link>
+                        <a  onClick={logoutHandler}><Nav.Link>logout</Nav.Link></a>
+                    </Nav>
+                    <Nav>
+                      <Button onClick={logoutHandler} variant="primary" className="mr-2">Logout</Button>
+                      <NavLink  to="/auth"><Button variant="primary" className="mr-2" >Cabinet</Button></NavLink>  
+    
+                    </Nav> 
+    
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    } else{
+        return(
+    
+            // <h1>AuthPage </h1>
+            <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
+                <Navbar.Brand>Logo</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link ><NavLink to="/">Home</NavLink></Nav.Link>
+                    <Nav.Link ><NavLink to="/cabinet">Cabinet</NavLink></Nav.Link>
+                        <a  onClick={logoutHandler}><Nav.Link>logout</Nav.Link></a>
+                    </Nav>
+                    <Nav>
+                      <NavLink to="/auth"><Button variant="primary" className="mr-2">Log in</Button></NavLink>  
+    
+                    </Nav> 
+    
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+
+    
 }
